8c14f389fee8f1e77496b8d82d41dfd80a5727ce
tanisha0605 <tanishakanal@gmail.com>
2024-05-17 16:53:09 +0530

    Added

--- Backtracking/Graph Colouring/GraphColoring.java
+++ Backtracking/Graph Colouring/GraphColoring.java
@@ -1,56 +1,53 @@
-public class GraphColoring {
-    static int[] colors;
-    static int[][] graph;
-    static int n; // number of vertices
-    static int m; // number of colors
 
-    static boolean nextValue(int k) {
-        do {
-            colors[k] = (colors[k] + 1) % (m + 1);
-            if (colors[k] == 0)
-                return false;
-            int j;
-            for (j = 0; j < n; j++) {
-                if (graph[k][j] != 0 && colors[k] == colors[j])
-                    break;
-            }
-            if (j == n)
-                return true;
-        } while (true);
+public class GraphColoring{
+    public static void addNode(int[][] graph, int x,int y)
+    {
+        graph[x][y]=1;
+        graph[y][x]=1;
     }
-
-    static void mColoring(int k) {
-        do {
-            if (!nextValue(k))
-                return;
-            if (k == n - 1) {
-                // All vertices colored
-                printColors();
-            } else {
-                mColoring(k + 1);
-            }
-        } while (true);
-    }
-
-    static void printColors() {
-        for (int i = 0; i < n; i++) {
-            System.out.print(colors[i] + " ");
+    public static boolean isSafe(int col,int[] color,int x,int[][] graph)
+    {
+        for(int i:graph[x])
+        {
+            if(graph[x][i]==1&&color[i]==col)return false; 
+            //If there exist a node between them 
+            //and color is same give false
         }
-        System.out.println();
+        return true;
     }
-
-    public static void main(String[] args) {
-        // Example graph represented as an adjacency matrix
-        n = 5;
-        m = 3;
-        graph = new int[][]{
-                {0, 1, 1, 1, 0},
-                {1, 0, 1, 0, 1},
-                {1, 1, 0, 1, 0},
-                {1, 0, 1, 0, 1},
-                {0, 1, 0, 1, 0}
-        };
-        colors = new int[n];
-        mColoring(0); // Start coloring from vertex 0
+    public static boolean canColor(int x,int[][] graph,int[] color,int n,int m)
+    {
+        if(x==n)return true; //Base case
+        for(int i=1;i<=m;i++)
+        {
+            if(isSafe(i,color,x,graph))
+            {
+                color[x]=i;
+                if (canColor(x+1,graph,color,n,m)) return true;
+                color[x]=0;
+            }
+        }
+        return false;
     }
+public static void main (String args[])
+{
+    int[][] graph=new int[4][4];
+    
+    addNode(graph,0,1);
+    addNode(graph,1,2);
+    addNode(graph,2,3);
+    addNode(graph,3,0);
+    addNode(graph,2,0);
+    //Dont populate diagonals
+    int[] color=new int[4];
+    boolean ans=canColor(0,graph,color,4,3); //4 is no of nodes,3 is max no colors,0 is startIndex
+    if(!ans)System.out.println("No we cant color");
+    else {
+        System.out.println("Yes we can color it as:");
+        for(int i:color)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
 }
---
+++ Backtracking/Hamiltonian Cycle/Hamiltonian.java
@@ -0,0 +1,63 @@
+import java.util.*;
+public class Hamiltonian{
+    public static void addNode(int[][] graph, int x,int y)
+    {
+        graph[x][y]=1;
+        graph[y][x]=1;
+    }
+    public static boolean isSafe(int x,int i,int[][] graph,int[] visit)
+    {
+        if(graph[visit[x-1]][i]==0)return false;
+        for(int j:visit)
+        {
+            if(j==i)return false;
+        }
+        return true;
+    }
+    public static boolean hamiltonCycle(int x,int[][] graph,int[] visit,int n)
+    {
+        if(x==n)
+        {
+            if(graph[visit[x-1]][visit[0]]==1)return true;
+            return false;
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(isSafe(x,i,graph,visit))
+            {
+                visit[x]=i;
+                if(hamiltonCycle(x+1,graph,visit,n))return true;
+                visit[x]=-1;
+            }
+        }
+        return false;
+    }
+    
+public static void main (String args[])
+{
+    int[][] graph=new int[5][5];
+    
+    addNode(graph,0,1);
+    addNode(graph,0,3);
+    addNode(graph,1,2);
+    addNode(graph,1,3);
+    addNode(graph,1,4);
+    addNode(graph,2,4);
+    addNode(graph,3,4);
+
+    //Dont populate diagonals
+    int[] visit=new int[5];
+    Arrays.fill(visit,-1);
+    visit[0]=0;
+    boolean ans=hamiltonCycle(1,graph,visit,5);
+    if(!ans)System.out.println("No there is no path");
+    else {
+        System.out.println("Yes we can iterate a path as:");
+        for(int i:visit)
+        {
+            System.out.print(i+" ");
+        }
+        System.out.print(visit[0]);
+    }
+}
+}
